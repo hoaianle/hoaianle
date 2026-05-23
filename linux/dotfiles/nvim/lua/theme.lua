85ec4cfd91352259771038456390ea9c56063dec
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2026-05-23 15:08:15 +0700

    add custom function to install neovim plugin

--- linux/dotfiles/nvim/lua/theme.lua
+++ linux/dotfiles/nvim/lua/theme.lua
@@ -1,26 +1,8 @@
--- Native Plugin Bootstrapper for the theme
-local pack_dir = vim.fn.stdpath("data") .. "/site/pack/plugins/start"
-local theme_path = pack_dir .. "/onedark.nvim"
+-- Import your custom plugin installer module
+local plugins = require('plugins')
 
--- If onedark folder doesn't exist, clone it from GitHub automatically
-if vim.fn.empty(vim.fn.glob(theme_path)) > 0 then
-  print("Downloading onedark.nvim...")
-  vim.cmd('redraw') 
-
-  vim.fn.system({
-    "git",
-    "clone",
-    "--depth", "1",
-    "https://github.com/navarasu/onedark.nvim.git",
-    theme_path
-  })
-  
-  vim.cmd([[packadd! onedark.nvim]])
-  
-  -- Clear the screen entirely
-  vim.cmd('redraw')
-  vim.api.nvim_command('echo ""')
-end
+-- Use your custom function to install the theme from GitHub
+plugins.install('navarasu/onedark.nvim')
 
 -- Configure and Load the Theme
 require('onedark').setup {

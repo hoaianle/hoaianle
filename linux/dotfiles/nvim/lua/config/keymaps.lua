08d019d9288baa3a627642fba70fcd6f3e044409
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2026-05-24 12:36:32 +0700

    remove delete buffer shortcut due to conflict

--- linux/dotfiles/nvim/lua/config/keymaps.lua
+++ linux/dotfiles/nvim/lua/config/keymaps.lua
@@ -5,6 +5,3 @@ vim.keymap.set('n', '<C-p>', ':Files<CR>', { silent = true })
 
 -- Search open buffers using fzf
 vim.keymap.set('n', '<leader>f', ':Buffers<CR>', { silent = true })
-
--- Delete current buffer
-vim.keymap.set('n', '<leader>d', ':bdelete<CR>', { silent = true })
